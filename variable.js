a890e857f20d1a3699113b7c2ea2f9a00dca6370
Gudiya1234567 <160309673+Gudiya1234567@users.noreply.github.com>
2024-02-27 14:13:40 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,40 @@
+
+//in var we can declre and initialize the variable with the same variable name
+
+
+
+var fname="gudiya";
+console.log(fname)
+
+
+var lname="vishwakarma";
+console.log(lname)
+
+var fname="roli";
+console.log(fname)
+
+
+var lname="singh";
+console.log(lname);
+
+//in let  we can  not declre but initialize the variable with the same variable name
+
+
+
+let fname="gudiya";
+console.log(fname);
+
+
+let fname="roli";
+ console.log(fname);
+
+ fname="roli";
+ console.log(fname);
+
+ //in const variable we can not write same variable name again and not give the same value again
+
+
+ const fruit="apple";
+ console.log(fruit);
+
+
