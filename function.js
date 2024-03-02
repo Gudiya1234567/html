076098cd5c167606f68d55b3ca97442132eaf416
gudiya vishwakarma <vgudiya490@gmail.com>
2024-03-02 10:46:43 +0530

    return

--- function.js
+++ function.js
@@ -1,7 +1,67 @@
-function myself(name,age){
-    return name+ " "+age
-}
+// function myself(name,age){
+//     return name+ " "+age
+// }
 
-let data= myself("Roli",14)
-console.log(data);
+// let data= myself("Roli",14)
+// console.log(data);
+
+
+
+
+
+// function add(a,b){
+//     result=(a+b);
+//     return result;
+    
+// }
+// console.log(add(3,6));
+// console.log(add(9,7));
+
+
+
+// function add(a,b){
+//     console.log(a-b)
+// }
+// add(3,5)
+
+// function myself(name,age){
+//     return name+ age;
+// }
+// console.log(myself("gudiya",20));
+
+
+
+
+// let price=30;
+// let affortable_price=20;
+// let profit=price-affortable_price;
+
+// function comfortable(){
+//     return profit
+// }
+// console.log(comfortable())
+
+
+
+// function myfunction(p,q){
+//     return p*q
+// }
+// // let num=myfunction(5,7)
+// let num1=myfunction(8,9)
+// console.log(num1)
+
+
+
+
+function modulus(i,j){
+    console.log(i%j)
+
+}
+modulus=(9,3)
+
+
+
+
+
+
 
---
+++ oprators.js
@@ -0,0 +1,27 @@
+let tshirt=300;
+let jeans=500;
+
+let add=tshirt+jeans;
+console.log(add)
+
+
+
+let sub=tshirt-jeans;
+console.log(sub);
+
+
+
+let  one_tshirt=500;
+let  six_tshirt=6;
+
+let count=one_tshirt*six_tshirt;
+console.log(count);
+
+let apple=900;
+let one_apple=4
+let divide=900/4
+console.log(divide)
+
+
+let modulous=900%7
+console.log(modulous);
